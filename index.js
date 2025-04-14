67dc6815d805810979ee50b8184ff5b967115274
Gourav Rathore <gouravrathore0304@gmail.com>
2025-04-14 12:54:36 +0530

    reverse a string function added

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+app.post('/functions/reverseString', async (req, res) => {
+  try {
+    const { input } = req.body;
+    if (typeof input !== 'string') {
+      throw new TypeError('Expected a string as input');
+    }
+    const output = input.split('').reverse().join('');
+    res.send({ output });
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+
+app.get('/functions/reverseString', (req, res) => {
+  res.send({
+    name: "reverseString",
+    description: "Reverses the given string",
+    input: {
+      type: "string",
+      description: "The string to reverse",
+      example: "hello"
+    },
+    output: {
+      type: "string",
+      description: "The reversed string",
+      example: "olleh"
+    }
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
